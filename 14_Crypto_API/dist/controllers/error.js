cd3f30efa112629c950f5b20af2be6358df1959c
MrsLecter <MissisLecter@protonmail.com>
2022-10-27 13:26:42 +0300

    refactor

--- 14_Crypto_API/dist/controllers/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.get404 = void 0;
-var get404 = function (req, res, next) {
-    res.status(404).json({ message: "Rout not found" });
-};
-exports.get404 = get404;
